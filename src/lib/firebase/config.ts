eb703c288c7ee4d720c3bc22aeb26c89414d0f2b
Sandra Bravo <sandrapbravom@gmail.com>
2025-07-17 19:56:42 +0000

    FRBS project: Inial setup using Firebase Studio
    Project variables configuration

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig: FirebaseOptions = {
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env. NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env. NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env. NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env. NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env. NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length && firebaseConfig.apiKey ? initializeApp(firebaseConfig) : getApp();
